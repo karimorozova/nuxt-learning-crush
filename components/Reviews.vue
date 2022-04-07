3af5c2ea03becbe6ac2872dc2a493926785b7bbf
karine-morozova <morozova_karine@ukr.net>
2022-04-07 12:46:46 +0300

    Summary

---
+++ components/Reviews.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Customer reviews</h3>
+        
+        <div v-if="!$fetchState.pending">
+            <ReviewCard 
+            v-for="reviewer in reviewers.results"
+            :key="reviewer.login.uuid"
+            :review="reviewer"
+            />
+        </div>
+        <div v-else><Loading class=""></Loading></div>
+    </div>
+</template>
+
+<script>
+   export default {
+    data() {
+      return {
+        reviewrs: []
+      }
+    },
+    async fetch () {
+      this.reviewers = await fetch(
+        'https://randomuser.me/api/?results=5'
+      ).then(res => res.json())
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/no-nav.vue
@@ -0,0 +1,11 @@
+ <template>
+<div class="container">
+ <NuxtLink to="/">Go back</NuxtLink>
+<Nuxt />
+</div>
+</template>
+<style scoped>
+.container {
+    padding: 15rem 0;
+}
+</style>
